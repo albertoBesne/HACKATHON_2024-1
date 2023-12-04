745918f5585f4fc42689e6ee6a31ebbca12cb269
albertoBesne <317012743@pcpuma.acatlan.unam.mx>
2023-12-04 13:39:33 -0600

    Initial Commit

---
+++ baseProyecto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  baseProyecto
+//
+//  Created by Alberto Besne Cabrera on 04/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ baseProyecto/baseProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  baseProyectoApp.swift
+//  baseProyecto
+//
+//  Created by Alberto Besne Cabrera on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct baseProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
