0fc5d9f66d0bbc600af69b546002e6f4839cd7ce
jatziri Berenice tenorio castelan <422074166@pcpuma.acatlan.unam.mx>
2023-12-06 15:49:49 -0600

    fixed design

---
+++ SignaAmbientVR/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  SignaAmbientVR
+//
+//  Created by CEDAM07 on 06/12/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
